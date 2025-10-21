c98ed19b8c42032911780154703ce442666291b7
Armando423344 <armandogomezmartin1@gmail.com>
2025-10-21 08:37:09 -0600

    practica 15

---
+++ practica 15/index.js
@@ -0,0 +1,10 @@
+ let form = document.getElementById('form');
+ let usuario = document.getElementById('usuario');
+ let contrasena = document.getElementById('contrasena');
+
+ usuario.value = "armandogomez ";
+ contrasena.value = "12345678";
+
+ function validateform(){
+    alert("Formulario enviado");
+ }
